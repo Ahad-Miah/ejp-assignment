267104b8eb5d6537c38198b7b732de9dd5a74b22
Ahad-Miah <ahadahmedcc@gmail.com>
2025-01-24 03:33:33 +0600

    deploy

--- src/app/myProfile/page.jsx
+++ src/app/myProfile/page.jsx
@@ -13,7 +13,7 @@ const MyProfile =async () => {
             <div className="bg-white shadow-lg rounded-lg p-6 max-w-lg w-full text-center">
                 <h1 className="text-3xl font-bold text-blue-600 mb-4">Welcome to Your Profile!</h1>
                 <p className="text-gray-700 text-lg mb-4">
-                    We're delighted to have you here. This is your personal space where you can explore updates, manage settings, and discover personalized features just for you!.
+                    We're delighted to have you here. This is your personal space where you can explore updates, manage settings, and discover personalized features just for you.
                 </p>
                 <p className="text-gray-600 text-base">
                     Stay connected and enjoy the seamless experience built with you in mind. Thank you for being a part of our journey!
